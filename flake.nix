62ff9a625c34b63f22f22193e3335c59cce0e535
Andrei Gužovski <and.guzovski@gmail.com>
2025-08-17 22:36:45 +0300

    init: initial commit with flake support

---
+++ flake.nix
@@ -0,0 +1,15 @@
+# /etc/nixos/flake.nix
+{
+  description = "My NixOS Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }: {
+    nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
