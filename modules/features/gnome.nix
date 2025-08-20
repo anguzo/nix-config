2a43d0378aa05223228f97f7dfc6ff56ec992b27
Andrei Gužovski <and.guzovski@gmail.com>
2025-08-20 18:53:58 +0300

    init: add file explorer to minimal GNOME setup

--- modules/features/gnome.nix
+++ modules/features/gnome.nix
@@ -19,6 +19,6 @@
   services.gnome.core-apps.enable = false;
   services.gnome.core-developer-tools.enable = false;
   services.gnome.games.enable = false;
-  environment.systemPackages = with pkgs; [ gnome-console ];
+  environment.systemPackages = with pkgs; [ gnome-console nautilus ];
   environment.gnome.excludePackages = with pkgs; [ gnome-tour gnome-user-docs ];
 }
